d3a91ae6307607f2553d2de96a4d424b8423d31f
Bob Naydenov <bob.naydenov@hotmail.com>
2018-03-12 01:48:49 +0000

    Update app with ad blocker

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
 const electron = require('electron')
 const {app, BrowserWindow, ipcMain} = require('electron')
+const { blockWindowAds, adBlocker } = require('electron-ad-blocker');
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
 
-let messengerWindow = null;
+let opggWindow = null;
 
 app.on('ready', function() {
 
   const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
 
-    messengerWindow = new BrowserWindow({
-        height: height * 0.50,
-        width: width * 0.45,
+    opggWindow = new BrowserWindow({
+        height: height * 0.8,
+        width: width * 0.6,
         frame: false,
         transparent: false,
         icon: path.join(__dirname, 'app/build/icon.png'),
         alwaysOnTop: false
     })
 
-    messengerWindow.loadURL('file://' + __dirname + '/app/index.html');
+    opggWindow.loadURL('file://' + __dirname + '/app/index.html');
 
     const options = {
       verbose: false,
       logger: console,
     }
 
-    //messengerWindow.webContents.openDevTools()
+    //opggWindow.webContents.openDevTools()
+
+    blockWindowAds(opggWindow, options);
 
 })
 
 ipcMain.on('button-press-hide', (event, arg) => {
-  messengerWindow.minimize()
+  opggWindow.minimize()
 })
 
 ipcMain.on('button-press-close', (event, arg) => {
